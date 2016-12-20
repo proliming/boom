e57f074c58412931ede1d6bc7dab7289b28c5189
liming16 <liming16@baidu.com>
2016-12-21 01:02:26 +0800

    Add startTime,endTime and change some variables name

--- harm.go
+++ harm.go
@@ -4,10 +4,12 @@ import "time"
 
 // Standards for the result by launching a Missile
 type Harm struct {
-    code      int        `json:"code"`
-    timestamp time.Time     `json:"timestamp"`
-    latency   time.Duration `json:"latency"`
-    bytesOut  uint64        `json:"bytes_out"`
-    bytesIn   uint64        `json:"bytes_in"`
+    startTime time.Time `json:"start_time"`
+    endTime time.Time `json:"end_time"`
+    statusCode      int        `json:"status_code"`
+    timestamp time.Time     `json:"timestamp"` // When a tick occur
+    latency   time.Duration `json:"latency"`   // Round Trip Latency
+    sentBytes  uint64        `json:"sent_bytes"`
+    receivedBytes   uint64        `json:"received_bytes"`
     error     string        `json:"error"`
 }
